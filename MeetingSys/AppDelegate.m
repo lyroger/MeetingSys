2b0b6fb403ecb521c3f9bb450c27c0f0bf37d538
luoyan <luoyan@tospur.com>
2017-06-08 17:28:46 +0800

    清楚提示数字

--- MeetingSys/AppDelegate.m
+++ MeetingSys/AppDelegate.m
@@ -31,6 +31,7 @@
     [self.rootViewController checkAppUpdateOnBackgroud:YES];
     [self.window makeKeyAndVisible];
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+    [JPUSHService setBadge:0];
     return YES;
 }
 
@@ -103,6 +104,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+    [JPUSHService setBadge:0];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
